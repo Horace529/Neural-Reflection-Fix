e0ba29c0b43e475a12a905c51fe54fa21a8f5765
ShangWen Yin <110508306+Horace529@users.noreply.github.com>
2026-09-02 14:31:09 +0800

    Create Renderer project

---
+++ Renderor/main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h> //用来访问 Windows API，比如创建窗口、处理消息
+#include <d3d12.h> // Direct3D 12 header
+#include <dxgi1_6.h> // DirectX Graphics Infrastructure header
+#include <d3dcompiler.h> // Direct3D shader compiler header
+#include <wrl.h> // 用来管理 COM 对象的智能指针
+#include <string> 
+#include <iostream>
+
+
